63c48021d7f83dd729a6c2c8a06132439da9b5a9
Özlem Yılmaz <65675763+ozlem55@users.noreply.github.com>
2022-02-22 17:49:27 +0300

    Add files via upload

---
+++ Siniflar/Siniflar/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Siniflar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //erişi belirleyiciler
+            //public
+            //private
+            //protected
+            Calısan calışan1 = new Calısan();
+            calışan1.Ad = "Özlem";
+            calışan1.Soyad = "yılmaz";
+            calışan1.No = 1234;
+            calışan1.Depertman = "Yazılım";
+            calışan1.CalısanBilgileri();
+
+            Console.WriteLine("*******************");
+
+            Calısan calışan2 = new Calısan();
+            calışan2.Ad = "Ayşe";
+            calışan2.Soyad = "Kara";
+            calışan2.No = 23445566;
+            calışan2.Depertman = "İnsan Kaynakları";
+            calışan2.CalısanBilgileri();
+        }
+    }
+    class Calısan
+    {
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Depertman;
+
+        public void CalısanBilgileri()
+        {
+            Console.WriteLine("Çalışan adı: " + Ad);
+            Console.WriteLine("Çalışan soyadı: " + Soyad);
+            Console.WriteLine("Çalışan numarası: " + No);
+            Console.WriteLine("Çalışanın depertmanı: " + Depertman);
+        }
+    }
+}
