529c2e375a809c4432babd5b92060806778e80eb
Özlem Yılmaz <65675763+ozlem55@users.noreply.github.com>
2022-03-01 15:18:39 +0300

    Add files via upload

---
+++ ToDoUygulamasi/ToDoUygulamasi/AllBoard.cs
@@ -0,0 +1,13 @@
+﻿namespace ToDoUygulamasi
+{
+    internal class AllBoard
+    {
+        internal object board;
+        private string v;
+
+        public AllBoard(string v)
+        {
+            this.v = v;
+        }
+    }
+}
